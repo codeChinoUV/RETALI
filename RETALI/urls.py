0415b466b4a81a21dcb1a2b0bac956e79e75564e
Ivan <irving_cena2@hotmail.com>
2020-11-19 13:03:46 -0600

    Primera versión de registro de usuario

--- RETALI/urls.py
+++ RETALI/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.usuarios.views import iniciarSesion, paginaInicio, cerrarSesion, registro
+from apps.usuarios.views import iniciarSesion, paginaInicio, cerrarSesion, registrarUsuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', iniciarSesion, name='login'),
     path('', paginaInicio, name='paginaInicio'),
     path('logout/', cerrarSesion, name='cerrarSesion'),
-    path('registro/', registro, name='registro'),
+    path('registro/',registrarUsuario , name='registro'),
 ]
