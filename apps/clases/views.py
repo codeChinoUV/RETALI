65d08dc317dc894267750cb29dbddc4a5a4e96c7
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-11-19 19:46:55 -0600

    Se empieza a trabajar con la información recibida del metodo POST de registrar clase

--- apps/clases/views.py
+++ apps/clases/views.py
@@ -8,3 +8,11 @@ def registrar_clase(request):
     if request.method == 'GET':
         form = ClaseForm()
         return render(request, 'RegistroClase.html', {'form': form})
+    elif request.method == "POST":
+        form = ClaseForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            datos = form.cleaned_data
+            print(datos.keys())
+            print("Hoooola")
+        return render(request, 'RegistroClase.html', {'form': form})
