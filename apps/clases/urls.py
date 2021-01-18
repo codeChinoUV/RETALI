7104ba93112a008f223195984ca040706d4ec19f
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2021-01-18 00:56:50 -0600

    Se crea el CU consultar grupo con la modificación del estado de la inscripcion del alumno

--- apps/clases/urls.py
+++ apps/clases/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from apps.usuarios.views import pagina_inicio
+from apps.usuarios.views import pagina_inicio, consultar_alumnos_de_clases, cambiar_estado_inscripcion_alumno
 from apps.clases.views import registrar_clase, informacion_clase, obtener_informacion_clase, unir_alumno_a_clase
 
 urlpatterns = [
@@ -9,6 +9,9 @@ urlpatterns = [
     path('obtener_informacion_clase/<str:codigo_clase>', obtener_informacion_clase, name='obtener_informacion_clase'),
     path('<str:codigo_clase>/', informacion_clase, name='ver_materia'),
     path('unirse_a_clase/<str:codigo_clase>', unir_alumno_a_clase, name='unirse_a_clase'),
+    path('<str:codigo_clase>/alumno/<int:id_alumno>/cambiar_estado_inscripcion',
+         cambiar_estado_inscripcion_alumno, name='cambiar_estado_inscripcion'),
+    path('<str:codigo_clase>/grupo', consultar_alumnos_de_clases, name='grupo'),
     path('', include('apps.actividades.urls')),
     path('', include('apps.foros.urls')),
     path('', include('apps.avisos.urls'))
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
-from apps.clases.models import Inscripcion, Alumno, Maestro
+from apps.clases.models import Inscripcion, Alumno, Maestro, Clase
 import re
 
 
@@ -60,8 +61,9 @@ def colocar_estado_inscripcion_clase(alumno, clases):
     :param clases: Las clases a colocar el estado
     :return: None
     """
-    for clase in clases:
-        clase.estado_inscipcion = alumno.inscripcion_set.filter(clase_id=clase.pk).first().aceptado
+    if clases is not None:
+        for clase in clases:
+            clase.estado_inscipcion = alumno.inscripcion_set.filter(clase_id=clase.pk).first().aceptado
 
 
 def _contar_cantidad_estado_de_clases(clases, datos_del_alumno):
@@ -74,13 +76,14 @@ def _contar_cantidad_estado_de_clases(clases, datos_del_alumno):
     datos_del_alumno['cantidad_clases_aceptado'] = 0
     datos_del_alumno['cantidad_clases_rechazado'] = 0
     datos_del_alumno['cantidad_clases_en_espera'] = 0
-    for clase in clases:
-        if clase.estado_inscipcion == 'Aceptado' and clase.abierta:
-            datos_del_alumno['cantidad_clases_aceptado'] += 1
-        elif clase.estado_inscipcion == 'Rechazado' and clase.abierta:
-            datos_del_alumno['cantidad_clases_rechazado'] += 1
-        elif clase.estado_inscipcion == 'En espera' and clase.abierta:
-            datos_del_alumno['cantidad_clases_en_espera'] += 1
+    if clases is not None:
+        for clase in clases:
+            if clase.estado_inscipcion == 'Aceptado' and clase.abierta:
+                datos_del_alumno['cantidad_clases_aceptado'] += 1
+            elif clase.estado_inscipcion == 'Rechazado' and clase.abierta:
+                datos_del_alumno['cantidad_clases_rechazado'] += 1
+            elif clase.estado_inscipcion == 'En espera' and clase.abierta:
+                datos_del_alumno['cantidad_clases_en_espera'] += 1
 
 
 def obtener_informacion_de_clases_de_maestro(maestro):
@@ -208,8 +211,121 @@ def validar_campos_no_vacios(username, password, nombre, apellidos, telefono):
     else:
         return True
 
-def validar_correo_no_registrado(correo):
 
+def validar_correo_no_registrado(correo):
     return True
 
     return False
+
+
+@login_required()
+def consultar_alumnos_de_clases(request, codigo_clase):
+    if request.method == "GET":
+        datos_del_maestro = obtener_informacion_de_clases_de_maestro(request.user.persona.maestro)
+        if _validar_existe_clase_maestro(request.user.persona.maestro, codigo_clase):
+            datos_del_maestro["clase_actual"] = Clase.objects.filter(abierta=True, codigo=codigo_clase).first()
+            datos_del_maestro["alumnos"] = _obtener_alumnos_inscritos_a_clase(datos_del_maestro["clase_actual"])
+            _contar_estados_inscripciones_de_alumnos(datos_del_maestro["alumnos"], datos_del_maestro)
+            return render(request, 'usuarios/consultar-alumnos-de-clase/ConsultarAlumnosDeClase.html',
+                          datos_del_maestro)
+        return render(request, 'generales/NoEncontrada.html', datos_del_maestro)
+    raise Http404
+
+
+def _obtener_alumnos_inscritos_a_clase(clase):
+    """
+    Recupera los alumnos de una clase y los devuelve con el estado de su inscripcion
+    :param clase: La clase de donde se tomaran los alumnos
+    :return: Una lista de alumnos
+    """
+    inscripciones = clase.inscripcion_set.all()
+    alumnos = []
+    if inscripciones is not None:
+        for inscripcion in inscripciones:
+            alumno = inscripcion.alumno
+            alumno.estado_inscripcion = inscripcion.aceptado
+            alumnos.append(alumno)
+        alumnos.sort(key=lambda x: x.apellidos)
+    return alumnos
+
+
+def _contar_estados_inscripciones_de_alumnos(alumnos, datos_del_maestro):
+    """
+    Cuenta la cantidad de alumnos dependiendo del estado de la inscripcion del alumno
+    :param alumnos: Los alumnos a contar
+    :param datos_del_maestro: Los datos en donde se agregara la informacion
+    :return: None
+    """
+    datos_del_maestro["cantidad_alumnos_aceptados"] = 0
+    datos_del_maestro["cantidad_alumnos_rechazados"] = 0
+    datos_del_maestro["cantidad_alumnos_en_espera"] = 0
+    if alumnos is not None:
+        for alumno in alumnos:
+            if alumno.estado_inscripcion == 'En espera':
+                datos_del_maestro["cantidad_alumnos_en_espera"] += 1
+            elif alumno.estado_inscripcion == 'Aceptado':
+                datos_del_maestro["cantidad_alumnos_aceptados"] += 1
+            elif alumno.estado_inscripcion == 'Rechazado':
+                datos_del_maestro["cantidad_alumnos_rechazados"] += 1
+
+
+def _validar_existe_clase_maestro(maestro, codigo_clase):
+    """
+    Valida que exista una clase del alumno
+    :param maestro: El maestro de a validr que la clase tenga la clase con el codigo
+    :param codigo_clase: El codigo de la clase a validar si existe
+    :return: True si la clase existe o False si no
+    """
+    existe_la_clase = False
+    clase = Clase.objects.filter(codigo=codigo_clase).first()
+    if clase is not None:
+        if maestro.clase_set.filter(abierta=True, pk=clase.pk).count() > 0:
+            existe_la_clase = True
+    return existe_la_clase
+
+
+@login_required()
+def cambiar_estado_inscripcion_alumno(request, codigo_clase, id_alumno):
+    if request.method == "POST":
+        if request.user.es_maestro:
+            if _validar_existe_inscripcion(id_alumno, codigo_clase):
+                if _validar_opcion_existente_inscripcion(request.POST['estado']):
+                    _actualizar_estado_inscripcion_alumno(codigo_clase, id_alumno, request.POST['estado'])
+                    return redirect('grupo', codigo_clase=codigo_clase)
+    raise Http404
+
+
+def _actualizar_estado_inscripcion_alumno(codigo_clase, id_alumno, estado_nuevo):
+    """
+    Actualiza el estado de la inscripcion de un alumno
+    :param estado_nuevo:
+    :param codigo_clase: El codigo de la clase de la inscripcion a actualizar
+    :param id_alumno: El id del alumno de actualizar el estado de su inscripción
+    :return: None
+    """
+    clase = Clase.objects.filter(codigo=codigo_clase).first()
+    Inscripcion.objects.filter(alumno_id=id_alumno, clase_id=clase.pk).update(aceptado=estado_nuevo)
+
+
+def _validar_opcion_existente_inscripcion(estado):
+    """
+    Valida si el estado se encuentra en la lista de estados validos
+    :param estado: El estado a validar
+    :return: True si se encuentra o False si no
+    """
+    estados = ["En espera", "Aceptado", "Rechazado"]
+    return estado in estados
+
+
+def _validar_existe_inscripcion(id_alumno, codigo_clase):
+    """
+    Valida si existe una inscripcion
+    :param id_alumno: El id del alumno a validar si existe la inscripcion
+    :param codigo_clase: El codigp de la clase a validar si existe en la inscripcion
+    :return: True si existe la inscripcion o False si no
+    """
+    existe_inscripcion = False
+    clase = Clase.objects.filter(codigo=codigo_clase).first()
+    if clase is not None:
+        existe_inscripcion = Inscripcion.objects.filter(alumno_id=id_alumno, clase_id=clase.pk).count() > 0
+    return existe_inscripcion
