1fec83fd281b09f06370ff8ddb87947eabe26c15
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2021-01-20 17:01:30 -0600

    Se cambia la ubicación de los static files y se termina la aplicación

--- RETALI/settings.py
+++ RETALI/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR / 'static/',
+    BASE_DIR / 'staticfiles/',
 )
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
