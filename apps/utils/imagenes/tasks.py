1020882f6fb126eb6a8888cca50d34b6c1303b1a
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2021-03-24 23:33:48 -0600

    Se modifica la tarea de create_thumbnail (funcional)

--- apps/utils/imagenes/tasks.py
+++ apps/utils/imagenes/tasks.py
@@ -3,52 +3,47 @@ Autor: José Miguel Quiroz Benitez
 Fecha de creación: 22-03-2021
 Ultima actualización: 22-03-2021
 """
+import io
 import os
-from zipfile import ZipFile
+import sys
 
 from celery import shared_task
 from django.conf import settings
 from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from apps.utils.imagenes.models import Imagen
+
+
+RESOLUCION_CALIDAD_BAJA = (128, 128)
+RESOLUCION_CALIDAD_MEDIA = (384, 384)
+RESOLUCION_CALIDAD_ALTA = (768, 768)
+
+CALIDAD_IMAGENES = 75
+
+LISTA_RESOLUCIONES = [RESOLUCION_CALIDAD_ALTA, RESOLUCION_CALIDAD_MEDIA, RESOLUCION_CALIDAD_BAJA]
+
 
 @shared_task()
-def make_thumbnails(imagen: Image, thumbnails: []):
+def make_thumbnails(imagen_id: int, thumbnails: [] = LISTA_RESOLUCIONES, quality: int = CALIDAD_IMAGENES):
+    """Crea imagenes de diferentes resoluciones de una Imagen"""
+    objeto_imagen = Imagen.objects.filter(pk=imagen_id).first()
+    imagen = objeto_imagen.imagen
     path, file = os.path.split(imagen.path)
     file_name, ext = os.path.splitext(file)
-    stream = imageField.open()
+    ext = ext.replace('.', '')
+    stream = imagen.open()
     imagen_bytes = Image.open(stream)
-    imagenes_generadas = []
     for w, h in thumbnails:
         img_copy = imagen_bytes.copy()
-        img_copy.thumbnail((w,h))
+        img_copy.thumbnail((w, h))
         img_copy_path = f'{file_name}_{w}_{h}.{ext}'
-        imagenes_generadas.append({'nombre': img_copy_path, 'archivo': img_copy})
-    imagen.close()
-    return imagenes_generadas
-
-
-
-#@shared_task()
-#def make_thumbnails(file_path, thumbnails=[] ):
-#    os.chdir(os.path.join(settings.MEDIA_ROOT, 'clases'))
-#    path, file = os.path.split(file_path)
-#    file_name, ext = os.path.splitext(file)
-#
-#    zip_file = f"{file_name}.zip"
-#    results = {'archive_path': f"{settings.MEDIA_URL}clases/{zip_file}"}
-#    try:
-#        img = Image.open(file_path)
-#        zipper = ZipFile(zip_file, 'w')
-#        zipper.write(file)
-#        os.remove(file_path)
-#        for w, h in thumbnails:
-#            img_copy = img.copy()
-#            img_copy.thumbnail((w,h))
-#            thumbnail_file = f'{file_name}_{w}_{h}.{ext}'
-#            img_copy.save(thumbnail_file)
-#            zipper.write(thumbnail_file)
-#            os.remove(thumbnail_file)
-#        img.close()
-#        zipper.close()
-#    except IOError as e:
-#        print(e)
-#    return results
+        output = io.BytesIO()
+        img_copy.save(output, format='JPEG', quality=quality)
+        output.seek(0)
+        imagen_a_guardar = InMemoryUploadedFile(output, 'ImageField', img_copy_path, 'img/jpeg',
+                                                sys.getsizeof(output), None)
+        imagen_calidad_nueva = Imagen(height=h, width=w, name=img_copy_path, imagen=imagen_a_guardar,
+                                      album_id=objeto_imagen.album_id)
+        imagen_calidad_nueva.save()
+    stream.close()
